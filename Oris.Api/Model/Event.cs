bd44c52912ea2212253d546b39df05cb4adfc027
Jan Palas <jan.palas@gmail.com>
2019-01-22 20:43:04 +0100

    Added JsonProperty definitions to Event model class

--- Oris.Api/Model/Event.cs
+++ Oris.Api/Model/Event.cs
@@ -5,14 +5,31 @@ namespace Oris.Api
 {
 	public class Event
 	{
+		[JsonProperty("ID")]
 		public int Id { get; set; }
+
+		[JsonProperty("Name")]
 		public string Name { get; set; }
+
+		[JsonProperty("Date")]
 		public DateTime Date { get; set; }
+
+		[JsonProperty("Region")]
 		public string Region { get; set; }
+
+		[JsonProperty("EntryDate1")]
 		public DateTime? EntryDate1 { get; set; }
+
+		[JsonProperty("EntryDate2")]
 		public DateTime? EntryDate2 { get; set; }
+
+		[JsonProperty("EntryDate3")]
 		public DateTime? EntryDate3 { get; set; }
+
+		[JsonProperty("Ranking")]
 		public string Ranking { get; set; }
+
+		[JsonProperty("Cancelled")]
 		public string Cancelled { get; set; }
 
 		[JsonProperty("GPSLat")]
@@ -21,10 +38,19 @@ namespace Oris.Api
 		[JsonProperty("GPSLon")]
 		public string GpsLongitude { get; set; }
 
+		[JsonProperty("Place")]
 		public string Place { get; set; }
+
+		[JsonProperty("Version")]
 		public int Version { get; set; }
+
+		[JsonProperty("ParentID")]
 		public int ParentId { get; set; }
+
+		[JsonProperty("Status")]
 		public string Status { get; set; }
+
+		[JsonProperty("OBPostupy")]
 		public string OBPostupy { get; set; }
 	}
 }
